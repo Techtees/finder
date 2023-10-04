e7d738e33fa6135c66d3aa211deb67b7ccfefadc
taiwoj121 <taiwoj121@gmail.com>
2023-10-04 16:59:07 +0100

    Global state management

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect} from 'react';
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+
+export const GithubProvider = ({children}) => {
+    
+    const [users, setUser] = useState([]) 
+    const [loading, setLoading] = useState(true)
+
+    
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${GITHUB_URL}/users`,
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        const data = await response.json()
+        setUser(data);
+        setLoading(false)
+    }
+
+  return (
+    <GithubContext.Provider 
+    value={{
+        users,
+        loading
+    }}
+    >
+        {children}
+    </GithubContext.Provider>
+  )
+}
+
+
+export default GithubContext
