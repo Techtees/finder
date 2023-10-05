b993b862d9d14c70ca365817f6d3717a297420b9
taiwoj121 <taiwoj121@gmail.com>
2023-10-05 15:30:16 +0100

    updted action reducer with loading action

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,14 +1,19 @@
 const githubReducer = (state, action) => {
-    switch(action.type) {
-        case 'GET_USERS':
-        return {
-            ...state,
-            users:action.payload,
-            loading: false
-        }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+      case 'GET_USERS':
+          return {
+              ...state,
+              users: action.payload,
+              loading: false, // Set loading to false when data is fetched
+          };
+      case 'SET_LOADING':
+          return {
+              ...state,
+              loading: true, // Set loading to true when fetching data
+          };
+      default:
+          return state;
+  }
+};
 
-export default githubReducer
+  export default githubReducer
