9bad8dad9f3339471f37719539b48037eba39e48
taiwoj121 <taiwoj121@gmail.com>
2023-10-05 16:56:56 +0100

    updated component with user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import UserResult from "../component/users/UserResults";
+import UserSearch from "../component/users/UserSearch";
 
 function Home(){
     return(
         <div>
-            <UserResult />
+            <UserSearch />
+            {/* <UserResult /> */}
         </div>
     )
 }
