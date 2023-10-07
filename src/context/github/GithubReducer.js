9a98974db26d5172e02db812ef96af2f3ee7dbf4
taiwoj121 <taiwoj121@gmail.com>
2023-10-07 18:44:21 +0100

    added single user to reducer

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,22 +1,30 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
-      case 'GET_USERS':
-          return {
-              ...state,
-              users: action.payload,
-              loading: false, // Set loading to false when data is fetched
-          };
-      case 'SET_LOADING':
-          return {
-              ...state,
-              loading: true, // Set loading to true when fetching data
-          };
-        case 'CLEAR_USERS':
-          return {
-              ...state,
-              users:[]
-          };
-      default:
+    case 'GET_USERS':
+        return {
+            ...state,
+            users: action.payload,
+            loading: false, // Set loading to false when data is fetched
+        };
+    case 'SET_LOADING':
+        return {
+            ...state,
+            loading: true, // Set loading to true when fetching data
+        };
+    case 'CLEAR_USERS':
+        return {
+            ...state,
+            users:[]
+        };
+    case 'GET_USER':
+        return {
+            ...state,
+            user: action.payload,
+            loading: false
+        }
+
+
+    default:
           return state;
   }
 };
