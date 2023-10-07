a2af5fdff78a53e82465907a2251ac4ef979091b
taiwoj121 <taiwoj121@gmail.com>
2023-10-07 18:43:34 +0100

    fetch single user

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -11,6 +11,7 @@ export const GithubProvider = ({children}) => {
     
     const initialState = {
         users: [],
+        user: {},
         loading: false,
     }
     const [state,dispatch] = useReducer(githubReducer, initialState)
@@ -41,6 +42,35 @@ export const GithubProvider = ({children}) => {
         })
      }
 
+
+    //  Get a single user
+     const getUser = async (login) => {
+
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        if(response.status === 404) {
+
+            window.location ='/notfound'
+        }else {
+
+            const data = await response.json()
+         
+    
+            dispatch({
+                type:'GET_USER',
+                payload: data,
+            })
+        }
+
+     }
+
     // Set loading
    const setLoading = () =>  dispatch({type: 'SET_LOADING'})
 
@@ -53,8 +83,10 @@ export const GithubProvider = ({children}) => {
     value={{
         users: state.users,
         loading: state.loading,
+        user:state.user,
         searchUsers,
         clearUser,
+        getUser,
     }}
     >
         {children}
